6d5d59e53c172c7a0869b83653c9163b3bc7e76a
Don Brightson <dbrightson78@gmail.com>
2023-03-01 13:39:50 +0530

    first commit

---
+++ js/auth.js
@@ -0,0 +1,29 @@
+
+window.addEventListener('DOMContentLoaded', function() {
+    function shouldRedirectToHomePage(user, isLoginPage) {
+      return user && isLoginPage;
+    }
+  
+    function shouldRedirectToLoginPage(user, isLoginPage) { 
+      return !user && !isLoginPage;
+    }
+  
+    CometChatWidget.init({
+      "appID": `${config.CometChatAppId}`,
+      "appRegion": `${config.CometChatRegion}`,
+      "authKey": `${config.CometChatAuthKey}`
+    }).then(response => {
+      CometChatWidget.CometChat.getLoggedinUser().then(
+        user => {
+            // check current page is the login page, or not.
+            const isLoginPage = window.location.href.includes('login');
+            if(shouldRedirectToHomePage(user, isLoginPage)){
+              window.location.href = '/';
+            }else if(shouldRedirectToLoginPage(user, isLoginPage)) {
+              window.location.href = '/login.html';
+            }
+        }, error => {
+        }
+      );
+    });
+  });
---
+++ js/config.js
@@ -0,0 +1,15 @@
+const config = {
+  apiKey: "AIzaSyCPxPZpLKn0KRhhCsF2WzWtqdzoNUSX0bU",
+  authDomain:"chat-app-d957d.firebaseapp.com",
+  databaseURL: "https://chat-app-d957d-default-rtdb.firebaseio.com/",
+  projectId: "chat-app-d957d",
+  storageBucket: "gs://chat-app-d957d.appspot.com",
+  messagingSenderId: "176280468823",
+  appId: "1:176280468823:web:50074305ec45def54e5212",
+  measurementId: "G-S8K7CBK9XM",
+  CometChatAppId: "23344543f4160630",
+  CometChatRegion: "US",
+  CometChatAuthKey: "a9bf6a208710b3eacf3d61c940de49c025eef45e",
+  CometChatAPIKey: "def56e0d5fd1f089e12bc25ffb12c1738398f3d6",
+  CometChatWidgetId: " d058e4f4-e5fe-44de-8318-e89398c177c2"
+};
