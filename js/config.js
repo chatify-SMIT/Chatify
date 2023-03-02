c318acdeb7316b9f5521dd0ed5d7d1008841fe6c
santhoshparthiban <santhoshparthiban2002@gmail.com>
2023-03-02 19:01:27 +0530

    added config.js

---
+++ js/config.js
@@ -0,0 +1,15 @@
+const config = {
+  apiKey: "AIzaSyBUCjqyLSNHt5z3AtB2VsU4MUPDINVfQCk",
+  authDomain: "chatify--smit.firebaseapp.com",
+  databaseURL: "https://chatify--smit-default-rtdb.firebaseio.com",
+  projectId: "chatify--smit",
+  storageBucket: "chatify--smit.appspot.com",
+  messagingSenderId: "530289849221",
+  appId: "1:530289849221:web:4820f2fe7f882e51b8caec",
+  measurementId: "G-DHFS5T7RTP",
+  CometChatAppId: "2339703596f32b10",
+  CometChatRegion: "US",
+  CometChatAuthKey: "c53f7be16ce9333cf3e69845e1870c4ed435ea67",
+  CometChatAPIKey: "086cdc92522f3b33b255b5655c85343dcbc29b8e",
+  CometChatWidgetId: "0ce7c17d-472e-4267-9383-936410fd276f"
+};
