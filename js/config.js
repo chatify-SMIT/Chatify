fa6261a4b187c6a0dd968d21bd411ca38b72428b
santhoshparthiban <santhoshparthiban2002@gmail.com>
2023-03-02 10:48:50 +0530

    upgrade chatify gmail and logo

--- js/config.js
+++ js/config.js
@@ -1,18 +1,18 @@
 
 
 const config = {
-  apiKey: "AIzaSyCPxPZpLKn0KRhhCsF2WzWtqdzoNUSX0bU",
-  authDomain:"chat-app-d957d.firebaseapp.com",
-  databaseURL: "https://chat-app-d957d-default-rtdb.firebaseio.com/",
-  projectId: "chat-app-d957d",
-  storageBucket: "gs://chat-app-d957d.appspot.com",
-  messagingSenderId: "176280468823",
-  appId: "1:176280468823:web:50074305ec45def54e5212",
-  measurementId: "G-S8K7CBK9XM",
-  CometChatAppId: "23344543f4160630",
+  apiKey: "AIzaSyBUCjqyLSNHt5z3AtB2VsU4MUPDINVfQCk",
+  authDomain: "chatify--smit.firebaseapp.com",
+  databaseURL: "https://chatify--smit-default-rtdb.firebaseio.com",
+  projectId: "chatify--smit",
+  storageBucket: "chatify--smit.appspot.com",
+  messagingSenderId: "530289849221",
+  appId: "1:530289849221:web:4820f2fe7f882e51b8caec",
+  measurementId: "G-DHFS5T7RTP",
+  CometChatAppId: "2339703596f32b10",
   CometChatRegion: "US",
-  CometChatAuthKey: "a9bf6a208710b3eacf3d61c940de49c025eef45e",
-  CometChatAPIKey: "def56e0d5fd1f089e12bc25ffb12c1738398f3d6",
-  CometChatWidgetId: "a5a0cb24-c430-479c-befe-51fb97956b1b"
+  CometChatAuthKey: "c53f7be16ce9333cf3e69845e1870c4ed435ea67",
+  CometChatAPIKey: "086cdc92522f3b33b255b5655c85343dcbc29b8e",
+  CometChatWidgetId: "0ce7c17d-472e-4267-9383-936410fd276f"
 };
 
